3a7262134bcb37fedba0292c9b9503027d85d433
Karen Vardanian <karenvardanian@MacBook-Pro-Karen.local>
2024-12-06 19:36:43 +0200

    choose model function update

--- index.ts
+++ index.ts
@@ -9,8 +9,16 @@ const bot = new TelegramBot(token, { polling: true });
 
 const openai = new OpenAI();
 
+const chatGptModels = {
+    "GPT-3.5 Turbo": "gpt-3.5-turbo",
+    "GPT-4": "gpt-4",
+    "GPT-4 Turbo": "gpt-4-turbo",
+    "GPT-4o mini": "gpt-4o-mini",
+    "GPT-4o": "gpt-4o"
+}
 class User {
     currentModel: string;
+    chooseModelMessageId: number | null = null;
     lastQuestion: string = "";
     lastCompletion: string = "";
     constructor(model?: string) {
@@ -58,42 +66,47 @@ async function start() {
         if (text === "/start") {
             return await bot.sendMessage(chatId, "Hello! I am AI ChatGPT bot. You can ask me some questions and I will answer!");
         } else if (text === "/model") {
+            const userInfo = usersInfo.get(chatId);
             const opts = {
                 reply_markup: {
                     inline_keyboard: [
                         [
                             {
                                 text: "GPT-3.5 Turbo",
-                                callback_data: "gpt-3.5-turbo"
+                                callback_data: chatGptModels["GPT-3.5 Turbo"]
                             },
                             {
                                 text: "GPT-4",
-                                callback_data: "gpt-4"
+                                callback_data: chatGptModels["GPT-4"]
                             },
                         ],
                         [
                             {
                                 text: "GPT-4 Turbo",
-                                callback_data: "gpt-4-turbo"
+                                callback_data: chatGptModels["GPT-4 Turbo"]
                             },
                             {
                                 text: "GPT-4o mini",
-                                callback_data: "gpt-4o-mini"
+                                callback_data: chatGptModels["GPT-4o mini"]
                             },
                         ],
                         [
                             {
-                            text: "GPT-4o",
-                            callback_data: "gpt-4o"
+                                text: "GPT-4o",
+                                callback_data: chatGptModels["GPT-4o"]
                             }
                         ]
                     ]
                 }
             };
-            return await bot.sendMessage(chatId, "GPT-3.5 Turbo - a fast, inexpensive model for simple tasks \n\n" +
+            return bot.sendMessage(chatId, "GPT-3.5 Turbo - a fast, inexpensive model for simple tasks \n\n" +
                 "GPT-4 and GPT-4 Turbo - the previous set of high-intelligence models \n\n" +
                 "GPT-4o mini - affordable and intelligent small model for fast, lightweight tasks \n\n" +
-                "GPT-4o - high-intelligence flagship model for complex, multi-step tasks", opts);
+                "GPT-4o - high-intelligence flagship model for complex, multi-step tasks", opts).then(msg => {
+                    if (userInfo) {
+                        userInfo.chooseModelMessageId = msg.message_id;
+                    }
+                });
         } else if (text === "/clear") {
             const userInfo = usersInfo.get(chatId);
             if (!userInfo) return;
@@ -123,15 +136,16 @@ async function start() {
             if (!userInfo) return;
             userInfo.lastQuestion = text;
             userInfo.lastCompletion = answer;
-            console.log(userInfo)
         }
     })
 
     bot.on("callback_query", async callback => {
         const userInfo = usersInfo.get(callback.from.id);
-        if (!userInfo) return;
+        if (!userInfo) throw Error("User info not found");
         if (!callback.data) throw Error("Callback data not found");
         userInfo.currentModel = callback.data;
+        if (!userInfo.chooseModelMessageId) throw Error("Choose model message id not found");
+        await bot.editMessageText("ChatGPT model set to " + Object.keys(chatGptModels).find(key => chatGptModels[key as keyof typeof chatGptModels] === callback.data), {chat_id: callback.from.id, message_id: userInfo.chooseModelMessageId});
     })
 }
 
